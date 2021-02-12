30507bb2ee968d1d842a52556031427ecc49d593
Timothy Smith <timothytsmith8@gmail.com>
2021-02-12 07:40:53 -0800

    Most of service-worker.js is complete. Need to figure out the error, however.

--- server.js
+++ server.js
@@ -14,12 +14,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/newApp',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
 
 // routes
 app.use(require("./routes/api.js"));
